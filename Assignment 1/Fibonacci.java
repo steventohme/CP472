262b35d3f0fcbc3ad3fd42cde09972f54b1bc201
Steven Tohme <steventohme59@gmail.com>
2024-01-17 08:51:38 -0500

    add fibonacci code

---
+++ Assignment 1/Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci {
+    public static void main(String[] args){
+
+        if (args.length <= 0) {
+			System.out.println("No command line arguments found.");
+        }
+
+        int n = Integer.parseInt(args[0]);
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2; i < n; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(fib[i]);
+        }
+    }
+}
