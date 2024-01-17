daacd7ca1a2d6ad11b075f48de735b4bc1b7a5a4
Steven Tohme <steventohme59@gmail.com>
2024-01-17 08:40:09 -0500

    add comments to bubble sort

--- Assignment 1/BubbleSort.py
+++ Assignment 1/BubbleSort.py
@@ -18,7 +18,9 @@ def bubble_sort(arr):
     n = len(arr)
 
     for i in range(n):
+        # Last i elements are already in place
         for j in range(0, n - i - 1):
+            # Swap if the element found is greater than the next element
             if arr[j] > arr[j+1] :
                 temp = arr[j]
                 arr[j] = arr[j+1]
