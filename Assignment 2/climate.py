9bdf5014ae9b1c2be67b5ca2b1be8bfc19513b84
Steven Tohme <steventohme59@gmail.com>
2024-02-12 08:41:50 -0500

    add ClimateData class

---
+++ Assignment 2/climate.py
@@ -0,0 +1,9 @@
+class ClimateData:
+
+    def __init__(self, date, max_gust, total_precipitation, min_temperature, max_temperature, avg_temperature):
+        self.date = date
+        self.max_gust = max_gust
+        self.total_precipitation = total_precipitation
+        self.min_temperature = min_temperature
+        self.max_temperature = max_temperature
+        self.avg_temperature = avg_temperature
