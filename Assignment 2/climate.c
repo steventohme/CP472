d62d47ffd55e7966197a063df6bbeacb1f35df79
Steven Tohme <steventohme59@gmail.com>
2024-02-12 15:28:51 -0500

    add parse line function

--- Assignment 2/climate.c
+++ Assignment 2/climate.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char date[11];
@@ -6,3 +9,18 @@ typedef struct {
     float minTemperature;
     float maxTemperature;
 } ClimateRecord;
+
+ClimateRecord* parse_line(char* line) {
+    ClimateRecord* record = malloc(sizeof(ClimateRecord));
+    char* token = strtok(line, ",");
+    strcpy(record->date, token);
+    token = strtok(NULL, ",");
+    record->maxGust = atoi(token);
+    token = strtok(NULL, ",");
+    record->totalPrecipitation = atof(token);
+    token = strtok(NULL, ",");
+    record->minTemperature = atof(token);
+    token = strtok(NULL, ",");
+    record->maxTemperature = atof(token);
+    return record;
+}
