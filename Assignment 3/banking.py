2995c74f490be1d524bbba2978b2f6b4812ad380
Steven Tohme <steventohme59@gmail.com>
2024-03-09 16:20:18 -0500

    add checking account

--- Assignment 3/banking.py
+++ Assignment 3/banking.py
@@ -8,3 +8,7 @@ class SavingsAccount(BankAccount):
     def __init__(self, accountNumber, accountHolderName, balance=0.0, minBalance=100.0):
         super().__init__(accountNumber, accountHolderName, balance)
         self.minBalance = minBalance
+
+class CheckingAccount(BankAccount):
+    def __init__(self, accountNumber, accountHolderName, balance=0.0):
+        super().__init__(accountNumber, accountHolderName, balance)
