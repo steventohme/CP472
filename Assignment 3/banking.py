122262634935743342e7d269e0fc542dfa28a228
Steven Tohme <steventohme59@gmail.com>
2024-03-09 16:20:07 -0500

    add savings account

--- Assignment 3/banking.py
+++ Assignment 3/banking.py
@@ -3,3 +3,8 @@ class BankAccount:
         self.accountNumber = accountNumber
         self.accountHolderName = accountHolderName
         self.balance = balance
+
+class SavingsAccount(BankAccount):
+    def __init__(self, accountNumber, accountHolderName, balance=0.0, minBalance=100.0):
+        super().__init__(accountNumber, accountHolderName, balance)
+        self.minBalance = minBalance
